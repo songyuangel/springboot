e28c0106235b5251d217de6c6556a0f62ed77aa3
songyuangel <309222152@qq.com>
2018-08-10 15:06:52 +0800

    init

---
+++ springBootBase/src/main/java/pers/song/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pers.song.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootBaseApplication.class);
+	}
+
+}
